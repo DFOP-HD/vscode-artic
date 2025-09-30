31b08ac423909459d45122e309880a0ef659f8e8
TimGrun <Tim.Grun@amd.com>
2025-09-30 14:22:29 +0200

    install on publish

--- publish.sh
+++ publish.sh
@@ -20,7 +20,7 @@ npm version patch --no-git-tag-version
 
 # Compile and package the extension
 echo "Compiling and packaging extension..."
-./package.sh
+./package.sh install
 
 # Get new version
 NEW_VERSION=$(node -p "require('./package.json').version")
