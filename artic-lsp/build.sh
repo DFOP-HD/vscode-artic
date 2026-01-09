d0f60a6afff2c2dfe54177416b2736a0307de559
TimGrun <Tim.Grun@amd.com>
2026-01-09 12:53:56 +0100

    export compile commands

--- artic-lsp/build.sh
+++ artic-lsp/build.sh
@@ -1,1 +1,3 @@
-cmake -S . -D CMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -B build && cmake --build build --parallel
+#!/bin/bash
+cd $(pwd)
+cmake -S . -D CMAKE_CXX_COMPILER=/usr/bin/clang++ -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja -B build && cmake --build build --parallel
