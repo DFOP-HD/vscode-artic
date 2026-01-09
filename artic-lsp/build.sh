f0b3cb08b848d3383600fa37d9ed962b55ab003d
TimGrun <Tim.Grun@amd.com>
2026-01-09 13:09:10 +0100

    fetch-content messages

--- artic-lsp/build.sh
+++ artic-lsp/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $(pwd)
-cmake -S . -D CMAKE_CXX_COMPILER=/usr/bin/clang++ -D CMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja -B build && cmake --build build --parallel
+cmake -S . -D CMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -B build && cmake --build build --parallel
